b867b66ef6de722e833db0b3129f896e5b688ed3
Jun <wnsgur4092@gmail.com>
2025-04-15 11:10:17 +0900

    Initialize project

---
+++ Project.swift
@@ -0,0 +1,34 @@
+import ProjectDescription
+
+let project = Project(
+    name: "Nottie",
+    targets: [
+        .target(
+            name: "Nottie",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "io.tuist.Nottie",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchScreen": [
+                        "UIColorName": "",
+                        "UIImageName": "",
+                    ],
+                ]
+            ),
+            sources: ["Nottie/Sources/**"],
+            resources: ["Nottie/Resources/**"],
+            dependencies: []
+        ),
+        .target(
+            name: "NottieTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "io.tuist.NottieTests",
+            infoPlist: .default,
+            sources: ["Nottie/Tests/**"],
+            resources: [],
+            dependencies: [.target(name: "Nottie")]
+        ),
+    ]
+)
